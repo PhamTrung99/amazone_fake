d549520494c5c7225699ce45277c1d08b6b8d56f
PhamTrung99 <teotu22@gmail.com>
2021-05-21 18:55:02 +0700

    setup connect neo4j

--- src/Controller/testConnect.con.js
+++ src/Controller/testConnect.con.js
@@ -1,8 +1,14 @@
-const { getAllProducts } = require("../Model/Mongodb/products.mongo");
+const mongoDB = require("../Model/Mongodb/products.mongo");
+const neoDB = require("../Model/Neo4j/comment.neo4j");
 
 const mongoTestCo = async (req, res) => {
-    const productList = await getAllProducts();
+    const productList = await mongoDB.getAllProducts();
     res.status(200).json(productList);
 }
 
-module.exports = {mongoTestCo}
+const neo4jTestCo = async (req, res) => {
+    const result = await neoDB.getAllComment();
+    res.status(200).json(result);
+}
+
+module.exports = {mongoTestCo, neo4jTestCo};
