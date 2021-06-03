28e01bdbd98f1daa9c36813738bccb4b131e870d
PhamTrung99 <teotu22@gmail.com>
2021-06-03 18:11:08 +0700

    config publif folder for route

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -7,6 +7,7 @@ module.exports =(app) => {
     app.use('/',express.static('src/public'));  // Only apply static folder for name index.ejs
     app.use('/productdetail',express.static('src/public')); 
     app.use('/cart',express.static('src/public'));  
+    app.use('/order',express.static('src/public'));  
     app.set("view engine", "ejs");        
     app.set("views",path.join(process.cwd() + "/src/Views"));
     app.use(bodyParser.urlencoded({ extended: false }));
