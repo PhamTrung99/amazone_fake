71aeed409d5b635064fe9e22a246fc62a9919b5a
PhamTrung99 <teotu22@gmail.com>
2021-06-03 18:14:10 +0700

    add post /getinfo

--- src/Routes/productDetail.route.js
+++ src/Routes/productDetail.route.js
@@ -3,5 +3,6 @@ const proDetailCon = require("../Controller/productDetail.con");
 const route = express.Router();
 
 route.get('/:objId', proDetailCon.productDetailCon);
+route.post('/getinfo', proDetailCon.getProduct);
 
 module.exports = route;
