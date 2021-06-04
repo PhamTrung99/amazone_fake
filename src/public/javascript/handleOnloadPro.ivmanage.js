98100b08323ed2c6d25a72a71b6304be25bed75e
PhamTrung99 <teotu22@gmail.com>
2021-06-04 09:10:50 +0700

    add html after receive response

--- src/public/javascript/handleOnloadPro.ivmanage.js
+++ src/public/javascript/handleOnloadPro.ivmanage.js
@@ -1,10 +1,23 @@
 function handleOnload() {
-    $.ajax({
-        type: "POST",
-        url: '/productdetail/getinfo',
-        data: _id
-    }).then((res) => {
-        document.getElementById("div_id").innerText = res.id;
-    })
+    let _id = document.currentScript.getAttribute('_id');
+    if (_id != '') {
+        $.ajax({
+            type: "POST",
+            url: '/productdetail/getinfo',
+            data: {
+                _id: _id
+            }
+        }).then((res) => {
+            document.getElementById("div_" + _id).innerHTML =
+            `<div class="thumbnail" style="display: inline-flex;height: 4rem;">
+            <img src="${res.image}" alt="">
+            <div class="caption">
+              <p>${res.name}</p>
+              <p>Price: ${res.price}</p>
+            </div>
+          </div>`;
+        })
+    }
+
 }
 handleOnload()
