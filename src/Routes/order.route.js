48cdcc42ac41208d281f220bbd415d4b7ba3d444
PhamTrung99 <teotu22@gmail.com>
2021-06-04 13:36:37 +0700

    add route post cancle order

--- src/Routes/order.route.js
+++ src/Routes/order.route.js
@@ -2,7 +2,10 @@ const express = require("express");
 const orderCon = require("../Controller/order.con");
 const route = express.Router();
 
+//rootpath : /order
+
 route.post('/confirmOrder',orderCon.orderCon);
 route.get('/orderManage',orderCon.orderManage);
+route.post('/cancleorder', orderCon.cancleOrder);
 
 module.exports = route;
