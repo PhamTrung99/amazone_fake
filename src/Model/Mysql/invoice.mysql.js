3f6c7b9fb47096d7634e6cade9cb64cdbc1ab906
PhamTrung99 <teotu22@gmail.com>
2021-06-04 14:11:03 +0700

    add update status, order_date when order again

--- src/Model/Mysql/invoice.mysql.js
+++ src/Model/Mysql/invoice.mysql.js
@@ -5,15 +5,18 @@ const addInvoice = async (invoice) => {
 }
 
 const getInvoice = async (userID) => {
-    return await db('invoice').where('USERID',userID);
+    return await db('invoice').where('USERID',userID).orderBy('ORDER_DATE','desc');
 }
 
 const deleteInvoice = async (orderID) => {
-    return await db('invoice').where('ID',orderID).update('IS_ACTIVE',0);
+    return await db('invoice').where('ID',orderID).update('IS_ACTIVE',0).update('STATUS','Đã huỷ');
 }
 
 const orderAgain = async (orderID) => {
-    return await db('invoice').where('ID',orderID).update('IS_ACTIVE',1);
+    return await db('invoice').where('ID',orderID)
+    .update('IS_ACTIVE',1)
+    .update('STATUS','Chờ xác nhận')
+    .update('ORDER_DATE',new Date());
 }
 
 module.exports = {
