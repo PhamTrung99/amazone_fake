4ad6a32d9b5b75965152672c14cb565f82086264
PhamTrung99 <1712848@student.hcmus.edu.vn>
2021-06-07 13:57:58 +0700

    nochange

--- src/Model/Mysql/invoice.mysql.js
+++ src/Model/Mysql/invoice.mysql.js
@@ -1,19 +1,19 @@
 const db = require('./connect.mysql');
 
 const addInvoice = async (invoice) => {
-    return await db('invoice').insert(invoice);
+    return await db(process.env.MYSQL_TABLE_INVOICE).insert(invoice);
 }
 
 const getInvoice = async (userID) => {
-    return await db('invoice').where('USERID',userID).orderBy('ORDER_DATE','desc');
+    return await db(process.env.MYSQL_TABLE_INVOICE).where('USERID',userID).orderBy('ORDER_DATE','desc');
 }
 
 const deleteInvoice = async (orderID) => {
-    return await db('invoice').where('ID',orderID).update('IS_ACTIVE',0).update('STATUS','Đã huỷ');
+    return await db(process.env.MYSQL_TABLE_INVOICE).where('ID',orderID).update('IS_ACTIVE',0).update('STATUS','Đã huỷ');
 }
 
 const orderAgain = async (orderID) => {
-    return await db('invoice').where('ID',orderID)
+    return await db(process.env.MYSQL_TABLE_INVOICE).where('ID',orderID)
     .update('IS_ACTIVE',1)
     .update('STATUS','Chờ xác nhận')
     .update('ORDER_DATE',new Date());
@@ -23,44 +23,3 @@ module.exports = {
     addInvoice,getInvoice,deleteInvoice,orderAgain
 };
 
-
-
-// module.exports = {
-//     all() {
-//         return db('users');
-//       },
-
-//       async single(id) {
-//         const users = await db('users').where('id', id);
-//         if (users.length === 0) {
-//           return null;
-//         }
-
-//         return users[0];
-//       },
-
-//       async singleByUserName(username) {
-//         const users = await db('users').where('username', username);
-//         if (users.length === 0) {
-//           return null;
-//         }
-
-//         return users[0];
-//       },
-
-//       add(user) {
-//         return db('users').insert(user);
-//       },
-
-//       patchRFToken(id, rfToken){
-//           return db('users').where('id',id).update('rfToken',rfToken);
-//       },
-
-//       async isValidRFToken(id, rfToken){
-//           const list = await db('users').where('id', id).andWhere('rfToken',rfToken);
-//           if(list.length >  0){
-//               return true;
-//           }
-//           return false;
-//       }
-// }
