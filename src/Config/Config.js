b1c0e06cab92590622ac66aedbe45a7b48fc00be
PhamTrung99 <1712848@student.hcmus.edu.vn>
2021-06-08 07:34:16 +0700

    add express-session options

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const session = require('express-session');
 
 
 module.exports =(app) => {
+    app.use(session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true}));
     app.use('/',express.static('src/public'));  // Only apply static folder for name index.ejs
     app.use('/productdetail',express.static('src/public')); 
     app.use('/cart',express.static('src/public'));  
